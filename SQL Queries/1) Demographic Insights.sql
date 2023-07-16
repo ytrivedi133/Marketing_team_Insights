82250d13cacbde05a9df848e19a3486b7191eafd
Yash Trivedi <37050561+ytrivedi133@users.noreply.github.com>
2023-07-16 00:30:00 -0400

    Update 1) Demographic Insights.sql

--- SQL Queries/1) Demographic Insights.sql
+++ SQL Queries/1) Demographic Insights.sql
@@ -1,17 +1,17 @@
 # 1) Demographic Insights:
-
+---------------------------------------------------------
 # a) Who prefers energy drink more? (male/female/non-binary?)
 select Gender, count(Respondent_ID) as total_respondents
 from dim_respondents
 group by gender
 order by total_respondents desc;
-
+---------------------------------------------------------
 # b) Which age group prefers energy drinks more? 
 select Age, count(Respondent_ID) as total_respondents
 from dim_respondents
 group by Age
 order by total_respondents desc;
-
+---------------------------------------------------------
 # c) Which type of marketing reaches the most Youth (15-30)?
 select Marketing_channels, count(Age) as total_youths
 from dim_respondents people
