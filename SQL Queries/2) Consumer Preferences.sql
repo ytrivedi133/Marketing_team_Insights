efd644b75d5dc14b36d2b81afc44aa3d0cdeff60
Yash Trivedi <37050561+ytrivedi133@users.noreply.github.com>
2023-07-16 00:30:27 -0400

    Update 2) Consumer Preferences.sql

--- SQL Queries/2) Consumer Preferences.sql
+++ SQL Queries/2) Consumer Preferences.sql
@@ -1,11 +1,11 @@
 # 2) Consumer Preferences:
-
+---------------------------------------------------------
 # a) What are the preferred ingredients of energy drinks among respondents?
 select Ingredients_expected as Ingredients, count(Respondent_ID) as total_respondents
 from fact_survey_responses
 group by Ingredients_expected
 order by total_respondents desc;
-
+---------------------------------------------------------
 # b) What packaging preferences do respondents have for energy drinks?
 select Packaging_preference, count(Respondent_ID) as total_respondents
 from fact_survey_responses
