ea168e09c3ebae7a21bf168df4a1b472ec345f02
Yash Trivedi <37050561+ytrivedi133@users.noreply.github.com>
2023-07-16 00:32:16 -0400

    Update 1) Demographic Insights.sql

--- SQL Queries/1) Demographic Insights.sql
+++ SQL Queries/1) Demographic Insights.sql
@@ -15,7 +15,7 @@ order by total_respondents desc;
 # c) Which type of marketing reaches the most Youth (15-30)?
 select Marketing_channels, count(Age) as total_youths
 from dim_respondents people
-	join fact_survey_responses surRes on surRes.Respondent_ID = people.Respondent_ID
+    join fact_survey_responses surRes on surRes.Respondent_ID = people.Respondent_ID
 where age in ('15-18', '19-30')
 group by Marketing_channels
 order by total_youths desc;
