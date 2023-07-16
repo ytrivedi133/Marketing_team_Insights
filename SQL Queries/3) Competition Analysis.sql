5181da4df3e725667ca53a5e5cbd0e4758dabd35
Yash Trivedi <37050561+ytrivedi133@users.noreply.github.com>
2023-07-16 00:30:55 -0400

    Update 3) Competition Analysis.sql

--- SQL Queries/3) Competition Analysis.sql
+++ SQL Queries/3) Competition Analysis.sql
@@ -1,11 +1,11 @@
 # 3) Competition Analysis:
-
+---------------------------------------------------------
 # a) Who are the current market leaders?
 select Current_brands, count(Respondent_ID) as total_respondents
 from fact_survey_responses
 group by Current_brands
 order by total_respondents desc;
-
+---------------------------------------------------------
 # b) What are the primary reasons consumers prefer those brands over ours? 
 with 
 	window_by_brands_and_reasons as (
@@ -25,7 +25,7 @@ with
 select *, concat(round(leader_brands_respondents_total/total_respondents_sum*100), '%') as total_percent_preference_of_4_leader_brands_over_CodeX
 from sum_of_respondents 
 order by total_percent_preference_of_4_leader_brands_over_CodeX desc;
-
+---------------------------------------------------------
 # c) What is one reason for consumers over choosing energy drink brands? How effective is it? 
 with window_by_brands_and_reasons as (
     select Reasons_for_choosing_brands, count(Respondent_ID) as total_respondents
