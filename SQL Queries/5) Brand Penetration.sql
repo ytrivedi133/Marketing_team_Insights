2316f162bc1f4d42755acf133a12c9236ba119d3
Yash Trivedi <37050561+ytrivedi133@users.noreply.github.com>
2023-07-16 00:40:02 -0400

    Update 5) Brand Penetration.sql

--- SQL Queries/5) Brand Penetration.sql
+++ SQL Queries/5) Brand Penetration.sql
@@ -8,17 +8,18 @@ from (
 	where Current_brands = "codex"
 	group by Brand_perception
 	order by total_respondents desc
-    ) as brand_perception;
+     ) as brand_perception;
 ---------------------------------------------------------
 # b) Which cities do we need to focus more on?
-with respondents_by_city as (
+with 
+    respondents_by_city as (
 	select Current_brands, City, count(fsr.Respondent_ID) as total_respondents
 	from dim_respondents dr 
-		join dim_cities dc on dc.City_ID = dr.City_ID
-		join fact_survey_responses fsr using (Respondent_ID)
+	    join dim_cities dc on dc.City_ID = dr.City_ID
+	    join fact_survey_responses fsr using (Respondent_ID)
 	where Current_brands = "codex"
 	group by City
 	order by total_respondents desc
-)
+    )
 select *, concat(round(total_respondents*100/sum(total_respondents) over ()), '%') as total_respondents_percentage
 from respondents_by_city;
