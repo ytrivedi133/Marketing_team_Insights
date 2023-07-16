6a02e9dbcfb0c80fdc9862d1cc669e3d79c2e205
Yash Trivedi <37050561+ytrivedi133@users.noreply.github.com>
2023-07-16 00:38:01 -0400

    Update 4) Marketing Channels and Brand Awareness.sql

--- SQL Queries/4) Marketing Channels and Brand Awareness.sql
+++ SQL Queries/4) Marketing Channels and Brand Awareness.sql
@@ -8,12 +8,13 @@ group by Marketing_channels
 order by respondents_by_channels desc;
 ---------------------------------------------------------
 # b) How effective are different marketing strategies and channels in reaching our customers?
-with top_marketing_channels_by_respondents as (
+with 
+    top_marketing_channels_by_respondents as (
 	select Current_brands, Marketing_channels, count(Respondent_ID) as total_respondents
 	from fact_survey_responses
 	where Current_brands = "CodeX"
 	group by Marketing_channels
-)
+    )
 select *, concat(round(total_respondents*100/total_respondents_of_CodeX), '%') as marketing_channel_percent_contribution
 from (
 	select *, sum(total_respondents) over() as total_respondents_of_CodeX
@@ -22,11 +23,12 @@ from (
 order by total_respondents desc;
 ---------------------------------------------------------
 # c) List each marketing channel for all the current brands and their effectiveness.
-with top_marketing_channels_by_respondents as (
+with 
+    top_marketing_channels_by_respondents as (
 	select Current_brands, Marketing_channels, count(Respondent_ID) as total_respondents, rank() over(partition by Current_brands order by count(Respondent_ID) desc) as top_marketing_channels
 	from fact_survey_responses
 	group by Marketing_channels, Current_brands
-)
+    )
 select *, concat(round(total_respondents*100/total_respondents_by_brand), '%') as marketing_channel_percent_contribution_by_brand
 from (
 	select *, sum(total_respondents) over(partition by Current_brands) as total_respondents_by_brand
